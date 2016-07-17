109bce22068f86a635e0ddddebebb48335b27711
brehonia <brehonia@gmail.com>
2016-07-18 00:02:22 +0100

    Halloween easter egg
    Occasionally turns everything spooky (9th visit in a session, and never again for that IP address)

--- index.php
+++ index.php
@@ -3,21 +3,59 @@
 	
 	$output = "";
 	$tooltip = "";
+	$cssfile = "bega.css";
+	$soundfile = "trumpet.ogg";
+	$pagetitle = "Lou Bega Names Your Baby";
+	$override = false;
 	
 	$db = mysqli_connect($config_mysqlHost, $config_mysqlUser, $config_mysqlPassword, $config_mysqlDatabase);
 	if (mysqli_connect_errno()) $output = "Oops I'm broken";
 	else
 	{
+		if (session_start())
+		{
+			if (!isset($_SESSION['visitcount']))
+			{
+				$_SESSION['visitcount'] = 1;
+			}
+			else
+			{
+				$_SESSION['visitcount']++;
+				if ($_SESSION['visitcount'] == 9)
+				{
+					if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $ipaddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
+					else $ipaddr = $_SERVER['REMOTE_ADDR'];
+					$ipaddr = mysqli_real_escape_string($db, $ipaddr);
+					
+					$q = mysqli_query($db, "select * from overrides where ipaddr = '" . $ipaddr . "'");
+					if (mysqli_num_rows($q) < 1)
+					{
+						$override = true;
+						mysqli_query($db, "insert into overrides (ipaddr) values ('" . $ipaddr . "')");
+					}
+				}
+			}
+		}
+		
 		$q = mysqli_query($db, "select * from babynames order by rand() limit 1");
 		$row = mysqli_fetch_assoc($q);
 		$output = $row['name'];
 		$tooltip = $row['meaning'];
+		if ($override) $output = "DRACUL";
+		
+		if ($output == "DRACUL")
+		{
+			$cssfile = "heck.css";
+			$soundfile = "eviltrumpet.ogg";
+			$pagetitle = "Lou Bega Consumes Your Life Essence";
+			$tooltip = "";
+		}
 	}
 ?>
 <html>
 <head>
-<title>Lou Bega Names Your Baby</title>
-<link rel="stylesheet" type="text/css" href="bega.css" />
+<title><?php echo $pagetitle; ?></title>
+<link rel="stylesheet" type="text/css" href="<?php echo $cssfile; ?>" />
 <link rel="stylesheet" type="text/css" href="tooltip.css" />
 <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="animateText.js"></script>
@@ -30,7 +68,7 @@
 </div>
 <div id="footer">by <a href="http://twitter.com/brehonia">@brehonia</a><br />for <a href="http://www.stoppodcastingyourself.com">SPY</a><br /><br />using data from <a href="http://introcs.cs.princeton.edu/java/data/">Princeton</a></div>
 <script type="text/javascript">
-		var noise = new Audio('trumpet.ogg');
+		var noise = new Audio('<?php echo $soundfile; ?>');
 		noise.play();
 		$("#babyname").animateText([{
 			offset: 0,
