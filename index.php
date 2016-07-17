7cb88054bbb07a78b51cc65113ef442bc15eec2b
brehonia <brehonia@gmail.com>
2016-07-17 23:48:42 +0100

    Show name meaning on tap or hover

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
+<?php
+	require_once('config.php');
+	
+	$output = "";
+	$tooltip = "";
+	
+	$db = mysqli_connect($config_mysqlHost, $config_mysqlUser, $config_mysqlPassword, $config_mysqlDatabase);
+	if (mysqli_connect_errno()) $output = "Oops I'm broken";
+	else
+	{
+		$q = mysqli_query($db, "select * from babynames order by rand() limit 1");
+		$row = mysqli_fetch_assoc($q);
+		$output = $row['name'];
+		$tooltip = $row['meaning'];
+	}
+?>
 <html>
 <head>
 <title>Lou Bega Names Your Baby</title>
 <link rel="stylesheet" type="text/css" href="bega.css" />
+<link rel="stylesheet" type="text/css" href="tooltip.css" />
 <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="animateText.js"></script>
+<script type="text/javascript" src="tooltip.js"></script>
 </head>
 <body>
 <div id="begabox">
-	<div id="namecontainer"><ul id="babyname"><li><?php
-		require_once('config.php');
-		$db = mysqli_connect($config_mysqlHost, $config_mysqlUser, $config_mysqlPassword, $config_mysqlDatabase);
-		if (mysqli_connect_errno()) echo "Oops I'm broken";
-		else
-		{
-			$q = mysqli_query($db, "select * from babynames order by rand() limit 1");
-			$row = mysqli_fetch_assoc($q);
-			echo $row['name'];
-		}
-	?></li></ul></div>
+	<div id="namecontainer"<?php if ($tooltip != "") echo ' rel="tooltip" title="'.$tooltip.'"'; ?>><ul id="babyname"><li><?php echo $output; ?></li></ul></div>
 	<div id="star"></div>
 </div>
 <div id="footer">by <a href="http://twitter.com/brehonia">@brehonia</a><br />for <a href="http://www.stoppodcastingyourself.com">SPY</a><br /><br />using data from <a href="http://introcs.cs.princeton.edu/java/data/">Princeton</a></div>
